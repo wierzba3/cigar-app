eb4fd92497e1b02e9a7f42f9f48857a88f7a5ef8
James Wierzba <jwierzba@communityinvestorsinc.com>
2017-02-05 12:50:04 -0700

    Add HumidorEntryGroup class

---
+++ Cigars/Cigars/Models/HumidorEntryGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cigars.Models
+{
+    public class HumidorEntryGroup
+    {
+
+        public int Quantity { get; set; }
+
+        public decimal AveragePrice { get; set; }
+
+    }
+}
